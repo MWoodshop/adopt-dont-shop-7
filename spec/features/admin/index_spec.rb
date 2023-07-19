b9fc52051a1c66e626398179ad224e6ed82082be
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-19 11:32:40 -0400

    Adds partial on applicants

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -80,4 +80,52 @@ RSpec.describe 'the admin/shelters' do
       expect(page).to have_content(shelter1.name)
     end
   end
+
+  # User Story 11 Test
+  it 'lists all the shelters with pending applications' do
+    shelter1 = Shelter.create(
+      name: 'Aurora Shelter',
+      city: 'Aurora, CO',
+      foster_program: true,
+      rank: 9
+    )
+
+    shelter2 = Shelter.create(
+      name: 'Beggin Strips Shelter',
+      city: 'Howell, NJ',
+      foster_program: false,
+      rank: 10
+    )
+
+    pet1 = Pet.create(
+      name: 'Dr. Dog',
+      age: 3,
+      breed: 'Husky',
+      shelter: shelter1,
+      adoptable: true
+    )
+
+    applicant1 = Applicant.create(
+      name: 'John Smith',
+      street_address: '1234 Example St',
+      city: 'Longmont',
+      state: 'CO',
+      zip_code: '80503',
+      description: 'I love pets!'
+    )
+
+    ApplicantsPet.create(applicant: applicant1, pet: pet1)
+    applicant1.reload
+
+    visit "/pets/#{pet1.id}"
+    expect(page).to have_content("Shelter Name: #{shelter1.name}")
+
+    visit "/applicants/#{applicant1.id}"
+    expect(page).to have_content('Status: Pending')
+
+    visit '/admin/shelters'
+    within('#pending-applications-title + p', text: shelter1.name) do
+      expect(page).to have_content(shelter1.name)
+    end
+  end
 end
