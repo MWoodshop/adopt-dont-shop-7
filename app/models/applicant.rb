58e818dc356dc082fcc3e641b347790898b97b3f
Captainlearyo <clayoleary@gmail.com>
2023-07-18 22:36:00 -0500

    update story 6

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -6,6 +6,6 @@ class Applicant < ApplicationRecord
   validates :street_address, presence: true
   validates :city, presence: true
   validates :zip_code, presence: true, numericality: true
-  validates :description, presence: true
+  #validates :description, presence: true
   validates :status, presence: true
 end
